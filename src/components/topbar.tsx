746338ecd2b88e83d2c56ad3d23d6774176df288
Mateus <mateus.venancio@basis.com.br>
2023-10-19 15:37:18 -0300

    initial commit

---
+++ src/components/topbar.tsx
@@ -0,0 +1,11 @@
+export default function TopBar() {
+    return (
+        <div className="px-4 bg-gray-100 shadow">
+            <div className="h-12 max-w-[--main-w] m-auto flex justify-between items-center">
+                <div className="font-bold">Carbon</div>
+                <div />
+                <div>Mateus</div>
+            </div>
+        </div>
+    );
+}
